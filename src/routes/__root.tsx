9d3870f00b61ff9fb44df58f592f286b18c3662f
Alif Nayem <alifnayem39@gmail.com>
2026-06-26 17:31:57 +0600

    Add favicon link to __root.tsx
    Added a favicon link to the HTML head section.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,6 +15,7 @@ export const Route = createRootRoute({
       },
     ],
     links: [
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
       {
